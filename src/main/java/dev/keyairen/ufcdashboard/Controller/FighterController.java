a395caa71574d3723fee0465d4584f44e9b21401
keyairen <179758374+keyairen@users.noreply.github.com>
2025-02-20 18:10:33 -0800

    Add backend for Fight API

--- src/main/java/dev/keyairen/ufcdashboard/Controller/FighterController.java
+++ src/main/java/dev/keyairen/ufcdashboard/Controller/FighterController.java
@@ -2,18 +2,19 @@ package dev.keyairen.ufcdashboard.Controller;
 
 import dev.keyairen.ufcdashboard.Repository.FightRepository;
 import dev.keyairen.ufcdashboard.Repository.FighterRepository;
+import dev.keyairen.ufcdashboard.model.Fight;
 import dev.keyairen.ufcdashboard.model.Fighter;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @CrossOrigin
 public class FighterController {
 
-    private FighterRepository fighterRepository;
-    private FightRepository fightRepository;
+    private final FighterRepository fighterRepository;
+    private final FightRepository fightRepository;
 
     public FighterController(FighterRepository fighterRepository, FightRepository fightRepository) {
         this.fighterRepository = fighterRepository;
@@ -27,9 +28,15 @@ public class FighterController {
         return fighter;
     }
 
-    @GetMapping("/health-check")
-    public String getHealthCheck() {
-        return "Situation Normal All Fired Up!";
+    @GetMapping("/fighter/{fighterName}/fights")
+    public List<Fight> getFightsForFighter(@PathVariable String fighterName, @RequestParam int year) {
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+        return this.fightRepository.getFightsByFighterBetweenDates(
+                fighterName,
+                startDate,
+                endDate
+        );
     }
 
 }
--- src/main/java/dev/keyairen/ufcdashboard/Repository/FightRepository.java
+++ src/main/java/dev/keyairen/ufcdashboard/Repository/FightRepository.java
@@ -1,15 +1,27 @@
 package dev.keyairen.ufcdashboard.Repository;
 
 import dev.keyairen.ufcdashboard.model.Fight;
+import org.springframework.cglib.core.Local;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface FightRepository extends CrudRepository<Fight, Long> {
 
     List<Fight> getByRedFighterOrBlueFighterOrderByDateDesc(String redFighter, String blueFighter, Pageable pageable);
+
+    @Query(
+            "select f from Fight f where (f.redFighter = :fighterName or f.blueFighter = :fighterName)"
+                    + " and f.date between :dateStart and :dateEnd order by f.date desc")
+    List<Fight> getFightsByFighterBetweenDates(
+            @Param("fighterName") String fighterName,
+            @Param("dateStart") LocalDate dateStart,
+            @Param("dateEnd") LocalDate dateEnd);
 
     default List<Fight> findLatestFightsByFighter(String fighterName, int count) {
         return getByRedFighterOrBlueFighterOrderByDateDesc(fighterName, fighterName, PageRequest.of(0, count));
