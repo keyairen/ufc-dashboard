5164bea8388cf14139c4283bccb07321a0ff84b0
keyairen <179758374+keyairen@users.noreply.github.com>
2025-02-20 18:22:17 -0800

    Add Fight Page to React App

--- src/frontend/src/pages/FightPage.js
+++ src/frontend/src/pages/FightPage.js
@@ -6,9 +6,29 @@ import {FightSmallCard} from "../components/FightSmallCard";
 
 export const FightPage = () => {
 
+
+    const [fights, setFights] = useState([]);
+    const { fighterName, year } = useParams();
+
+    useEffect(() => {
+        const fetchFights = async () => {
+            const response = await fetch(`http://localhost:8080/fighter/${fighterName}/fights?year=${year}`);
+            const data = await response.json();
+            setFights(data);
+
+        };
+        fetchFights();
+
+    }, []
+    );
+
+
     return (
         <div className="FightPage">
             <h1>Fight Page</h1>
+            {
+                fights.map(fight => <FightDetailCard fighterName={fighterName} fight={fight} />)
+            }
         </div>
     );
 }
